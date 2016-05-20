91ea8d13c8611e529cbd4e05909585df8155834f
Edmund Korley <korley.em@gmail.com>
2016-05-20 17:35:52 -0400

    React module

---
+++ app/components/Table.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+var contracts = require('json!./content.json');
+
+export default class Table extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        let headers = Object.keys(contracts[0]);
+        let thDivs = headers.map((category) => <th>{category}</th>);
+        let tdDivs = contracts.map((review) => {
+            let tdData = Object.keys(review).map((key) => {
+                return <td>{ review[key] }</td>
+            });
+            return <tr>
+                {tdData}
+            </tr>
+        });
+        return <table>
+            <thead>
+                <tr>
+                    {thDivs}
+                </tr>
+            </thead>
+            <tbody>
+                {tdDivs}
+            </tbody>
+        </table>
+    }
+}
---
+++ app/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Base from './components/Table.jsx';
+
+render(<Base />, document.getElementById('contracts-table'));
