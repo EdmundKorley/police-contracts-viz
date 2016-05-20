fb215d36304524f6da1a1f9f741c21ffc4395794
Edmund Korley <korley.em@gmail.com>
2016-05-20 18:29:25 -0400

    Rehashed json layout

--- dataUtility.py
+++ dataUtility.py
@@ -1,8 +1,22 @@
 import csv
 import json
 
+def dataByState(data):
+    new = {}
+    for item in data:
+        state = item['Contract City/State']
+        sign = new.get(state, None)
+        if sign is None:
+            new[state] = []
+        else:
+            sign.append(item)
+            new[state] = sign
+    return new
+
 with open('data/content.csv') as csvFile:
     data = list(csv.DictReader(csvFile))
     for item in data:
         item.pop('', None)
-    json.dump(data, open('raw/content.json', 'wb'))
+    newData = dataByState(data)
+    with open('app/components/rehash.json', 'w') as f:
+        json.dump(newData, f, ensure_ascii=False)
